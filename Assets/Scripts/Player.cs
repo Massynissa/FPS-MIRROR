532e3b797ef0e969ef9d94b1fb03e375d24a2e2b
Massynissa <samy.benyoucef@live.fr>
2025-02-22 17:58:13 +0100

    7.5
    pause golo

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,25 +3,78 @@ using Mirror;
 
 public class Player : NetworkBehaviour
 {
+    [SyncVar]
+    private bool _isDead = false;
+    public bool isDead
+    {
+        get { return _isDead; }
+        protected set { _isDead = value; }
+    }
+
     [SerializeField]
     private float maxHealth = 100f;
 
     [SyncVar]
     private float currentHealth;
 
-    private void Awake()
+    [SerializeField]
+    private Behaviour[] disableOnDeath;
+    private bool[] wasEnableOnStart;
+
+    public void Setup()
     {
+        wasEnableOnStart = new bool [disableOnDeath.Length];
+        for(int i = 0; i < disableOnDeath.Length; i++)
+        {
+            wasEnableOnStart[i] = disableOnDeath[i].enabled;
+        }
+
         SetDefaults();
     }
 
     public void SetDefaults()
     {
+        isDead = false;
         currentHealth = maxHealth;
+
+        for(int i = 0; i < disableOnDeath.Length; i++)
+        {
+            disableOnDeath[i].enabled = wasEnableOnStart[i];
+        }
+        Collider col = GetComponent<Collider>();
+        if(col != null)
+        {
+            col.enabled = true;
+        }
+
     }
 
-    public void TakeDamage(float amount)
+    [ClientRpc]
+    public void RpcTakeDamage(float amount)
     {
+        if (isDead)
+        {
+            return;
+        }
+
         currentHealth -= amount;
         Debug.Log(transform.name + " a maintenant : " + currentHealth + " points de vie.");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        isDead = true;
+
+        for(int i = 0; i < disableOnDeath.Length; i++)
+        {
+            disableOnDeath[i].enabled = false;
+        }
+
+        Debug.Log(transform.name + " a été éliminé."); //20:44 ep8
     }
 }
