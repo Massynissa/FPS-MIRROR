6b5e2a2645d6c1e9d6731fa8369a792b4735dde4
Massynissa <samy.benyoucef@live.fr>
2025-02-23 16:20:00 +0100

    player visuals 9
    adds :
    player visuals
    jetpack animations
    jetpack particles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 [RequireComponent(typeof(PlayerMotor))]
 [RequireComponent(typeof(ConfigurableJoint))]
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
@@ -24,24 +25,29 @@ public class PlayerController : MonoBehaviour
 
     private PlayerMotor motor;
     private ConfigurableJoint joint;
+    private Animator animator;
 
     private void Start()
     {
         motor = GetComponent<PlayerMotor>();
         joint = GetComponent<ConfigurableJoint>();
         SetJointSettings(jointSpring);
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
         // Calculer la vélocité du mouvement de notre joueur
-        float xMov = Input.GetAxisRaw("Horizontal");
-        float zMov = Input.GetAxisRaw("Vertical");
+        float xMov = Input.GetAxis("Horizontal");
+        float zMov = Input.GetAxis("Vertical");
 
         Vector3 moveHorizontal = transform.right * xMov;
         Vector3 moveVertical = transform.forward * zMov;
 
-        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+        Vector3 velocity = (moveHorizontal + moveVertical) * speed;
+
+        //Jouer animation thruster
+        animator.SetFloat("ForwardVelocity", zMov);
 
         motor.Move(velocity);
 
